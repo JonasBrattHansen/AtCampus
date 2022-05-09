55b5d6697793f59f8b35d38277aae0d8b0e3e5fe
tomrarcher <syvertcollege@gmail.com>
2022-05-10 00:55:47 +0200

    Created all tables for Postgres in Flyway

--- Server/src/main/resources/db/migration/V01__table_setup.sql
+++ Server/src/main/resources/db/migration/V01__table_setup.sql
@@ -1,12 +1,64 @@
-CREATE TABLE Users (
+CREATE TABLE programs (
+    program_id bigserial PRIMARY KEY,
+    program_name varchar(100) NOT NULL
+);
+
+CREATE TABLE schools (
+    school_id bigserial PRIMARY KEY,
+    school_name varchar(100) NOT NULL
+);
+
+CREATE TABLE users (
     user_id bigserial PRIMARY KEY,
     user_first_name varchar(30) NOT NULL,
     user_last_name varchar(40) NOT NULL,
     user_email varchar(100) NOT NULL,
     user_password varchar(255) NOT NULL,
     user_phone_number varchar(15) NOT NULL,
-    user_school bigint NOT NULL,
-    user_program bigint NOT NULL,
+    user_school bigint NOT NULL references schools(school_id),
+    user_program bigint NOT NULL references programs(program_id),
     user_profile_image varchar(255),
     user_date_created date not null default current_date
-)
+);
+
+CREATE TABLE groups (
+    group_id bigserial PRIMARY KEY,
+    group_name varchar(100) NOT NULL,
+    group_description TEXT,
+    group_image varchar(255),
+    group_school bigint references schools(school_id),
+    group_admin bigint references users(user_id),
+    group_date_created date not null default current_date
+);
+
+CREATE TABLE group_request (
+    group_request_id bigserial PRIMARY KEY,
+    user_id bigint NOT NULL references users(user_id),
+    group_id bigint NOT NULL references groups(group_id)
+);
+
+CREATE TABLE user_group (
+    user_group_id bigserial PRIMARY KEY,
+    user_id bigint NOT NULL references users(user_id),
+    group_id bigint NOT NULL references groups(group_id),
+    is_favorite bool NOT NULL default false,
+    date_joined date NOT NULL default current_date
+);
+
+CREATE TABLE post (
+    post_id bigserial PRIMARY KEY,
+    post_title varchar(70) NOT NULL,
+    post_body text NOT NULL,
+    post_date_created date NOT NULL default current_date,
+    user_id bigint NOT NULL references users(user_id),
+    group_id bigint NOT NULL references groups(group_id)
+);
+
+CREATE TABLE comment (
+    comment_id bigserial PRIMARY KEY,
+    comment_body text NOT NULL,
+    comment_date date NOT NULL default current_date,
+    comment_post_id bigint NOT NULL references post(post_id),
+    comment_user_id bigint NOT NULL references users(user_id)
+);
+
