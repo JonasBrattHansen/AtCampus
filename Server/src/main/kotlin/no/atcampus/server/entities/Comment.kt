2e3c9fa7218026329a6a459a5741fa83454b075d
tomrarcher <syvertcollege@gmail.com>
2022-05-10 12:31:59 +0200

    Added more entities

---
+++ Server/src/main/kotlin/no/atcampus/server/entities/Comment.kt
@@ -0,0 +1,22 @@
+package no.atcampus.server.entities
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.SequenceGenerator
+import javax.persistence.Table
+
+@Entity
+@Table(name = "comment")
+class Comment (
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comments_comment_sequence_id")
+    @SequenceGenerator(name = "comments_comment_sequence_id", allocationSize = 1)
+    @Column(name = "comment_id")
+    val id: Long? = null,
+    @Column(name = "comment_body")
+    val body: String,
+
+)
--- Server/src/main/kotlin/no/atcampus/server/entities/Post.kt
+++ Server/src/main/kotlin/no/atcampus/server/entities/Post.kt
@@ -8,30 +8,33 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.JoinColumn
+import javax.persistence.ManyToMany
 import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
 import javax.persistence.SequenceGenerator
 import javax.persistence.Table
 
 @Entity
-@Table(name = "user_group")
-class UserGroup (
+@Table(name = "post")
+class Post (
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_group_id_sequence")
-    @SequenceGenerator(name = "user_group_id_sequence", allocationSize = 1)
-    @Column(name = "user_group_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "posts_post_id_sequence")
+    @SequenceGenerator(name = "posts_post_id_sequence", allocationSize = 1)
+    @Column(name = "post_id")
     val id: Long? = null,
+    @Column(name = "post_title")
+    val title: String,
+    @Column(name = "post_bodu")
+    val body: String,
+    val dateCreated: LocalDate? = LocalDate.now(),
     @ManyToOne(cascade = [CascadeType.ALL])
     @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     val user: User,
     @ManyToOne(cascade = [CascadeType.ALL])
     @JoinColumn(name = "group_id", referencedColumnName = "group_id")
-    val group: Group,
-    @Column(name = "is_favorite")
-    val favorite: Boolean? = false,
-    @Column(name = "date_joined")
-    val dateJoined: LocalDate? = LocalDate.now()
+    val group: Group
 ){
     override fun toString(): String {
-        return "UserGroup(id=$id, user=$user, group=$group, favorite=$favorite, dateJoined=$dateJoined)"
+        return "Post(id=$id, title='$title', body='$body', dateCreated=$dateCreated, user=$user, group=$group)"
     }
 }
