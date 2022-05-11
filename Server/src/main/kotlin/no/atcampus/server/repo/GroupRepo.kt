14e83ce4d96217a3a2f86eeca6b22a8d273907f6
tomrarcher <syvertcollege@gmail.com>
2022-05-11 13:30:55 +0200

    Finished GroupRepo.kt

--- Server/src/main/kotlin/no/atcampus/server/repo/GroupRepo.kt
+++ Server/src/main/kotlin/no/atcampus/server/repo/GroupRepo.kt
@@ -1,8 +1,12 @@
 package no.atcampus.server.repo
 
 import no.atcampus.server.entities.Group
+import no.atcampus.server.entities.School
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GroupRepo : JpaRepository<Group, Long>
+interface GroupRepo : JpaRepository<Group, Long>{
+    fun findGroupsByName(name: String): MutableList<Group>
+    fun findGroupsBySchool(school: School): MutableList<Group>
+}
