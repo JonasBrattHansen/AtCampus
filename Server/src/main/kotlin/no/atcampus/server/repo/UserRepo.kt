c09a3e45eee96a73e4256dee083378ff81782770
Sakelig <71970061+Sakelig@users.noreply.github.com>
2022-05-11 13:35:59 +0200

    userRepo done

--- Server/src/main/kotlin/no/atcampus/server/repo/UserRepo.kt
+++ Server/src/main/kotlin/no/atcampus/server/repo/UserRepo.kt
@@ -1,10 +1,14 @@
 package no.atcampus.server.repo
 
+import no.atcampus.server.entities.Program
+import no.atcampus.server.entities.School
 import no.atcampus.server.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepo : JpaRepository<User, Long>{
-
+    fun findUserByEmail(email: String): User
+    fun findUsersBySchool(school: School): MutableList<User>
+    fun findUsersByProgram(program: Program): MutableList<User>
 }
