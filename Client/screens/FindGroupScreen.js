19086c8e91398e345f24be713cebecd8f1e4be50
Marco Gravbrøt <m.gravbrot@gmail.com>
2022-05-13 13:59:15 +0200

    Finished find group screen

--- Client/screens/FindGroupScreen.js
+++ Client/screens/FindGroupScreen.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import {FlatList, StyleSheet, Text, View} from "react-native";
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import CreateGroup from "../components/CreateGroup";
 import GroupPreview from "../components/GroupPreview";
 import PostPreview from "../components/PostPreview";
 import WideGroupPreview from "../components/WideGroupPreview";
+import {AntDesign} from "@expo/vector-icons";
 
 const groupPreviews = [
 	{
@@ -48,11 +49,36 @@ function Separator() {
 }
 
 function FindGroupScreen(props) {
+	const [search, setSearch] = useState("");
+	
 	return (
 		<View style={styles.container}>
 			<FlatList
 				contentContainerStyle={styles.postPreviews}
 				data={groupPreviews}
+				ListHeaderComponent={
+					<View style={styles.header}>
+						<View style={styles.searchBarWrapper}>
+							<AntDesign
+								style={styles.searchIcon}
+								name={"search1"}
+								size={18}
+								color={"black"}
+							/>
+							
+							<TextInput
+								style={styles.searchBar}
+								placeholder={"Search for a group.."}
+								value={search}
+								onChangeText={text => setSearch(text)}
+							/>
+						</View>
+						
+						<TouchableOpacity style={styles.filterButton}>
+							<Text>Filter</Text>
+						</TouchableOpacity>
+					</View>
+				}
 				ItemSeparatorComponent={Separator}
 				renderItem={({item}) =>
 					<WideGroupPreview
@@ -78,6 +104,38 @@ const styles = StyleSheet.create({
 	separator: {
 		height: 15,
 	},
+	header: {
+		display: "flex",
+		alignItems: "flex-start",
+		marginBottom: 20,
+	},
+	searchBarWrapper: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		marginBottom: 10,
+		backgroundColor: "rgb(240, 240, 240)",
+		borderRadius: 15,
+	},
+	searchIcon: {
+		padding: 15,
+	},
+	searchBar: {
+		flex: 1,
+		height: "100%",
+		//borderWidth: 1,
+		//borderColor: "rgb(230, 230, 230)",
+		fontSize: 16,
+		borderRadius: 10,
+	},
+	filterButton: {
+		display: "flex",
+		alignItems: "center",
+		backgroundColor: "rgb(240, 240, 240)",
+		paddingVertical: 10,
+		paddingHorizontal: 15,
+		borderRadius: 20,
+	}
 })
 
 export default FindGroupScreen;
