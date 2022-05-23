54be9db2d9d1b24e67524d11d434b48d0b75b7f8
tomrarcher <syvertcollege@gmail.com>
2022-05-23 15:03:51 +0200

    kanskje?

--- Client/services/UserService.js
+++ Client/services/UserService.js
@@ -1,8 +1,9 @@
 import AxiosService from "./AxiosService";
+import {getAllUserGroups} from "./GroupService";
 
 const instance = AxiosService.getInstance();
 
 export async function getUserIdByEmail(email){
     let userDetails = await instance.get(`/api/user/email/${email}`)
-    console.log(userDetails.data)
+    return await getAllUserGroups(userDetails?.id)
 }
