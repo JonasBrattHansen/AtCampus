d12e8f303f0e4a02662f3a96bb032f4cf818177d
tomrarcher <syvertcollege@gmail.com>
2022-05-27 13:57:11 +0200

    added basic groupcomment functionality

--- Client/screens/GroupScreen.js
+++ Client/screens/GroupScreen.js
@@ -156,7 +156,7 @@ function GroupScreen({navigation, route}) {
                             title={item.title}
                             preview={item.body}
                             date={item.date}
-                            onPress={() => navigation.navigate("Group Comment")}
+                            onPress={() => navigation.navigate("Group Comment", {post: item})}
                         />
                     }
                 />
