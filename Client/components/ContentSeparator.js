d8a0a6c7e7feb46b629feab3100b68c28ae7dd70
Cooljean <Cooljean95@ymail.com>
2022-06-03 13:58:17 +0200

    changed the design.

--- Client/components/ContentSeparator.js
+++ Client/components/ContentSeparator.js
@@ -3,18 +3,9 @@ import { StyleSheet, View } from 'react-native'
 
 
 const ContentSeparator = (border) => (
-    <View style={styles.container}>
-        <View style={{ borderColor: "#7c7fca", borderWidth: border }} />
+    <View style={{ borderColor: "#f1f1f1", borderWidth: border }}>
     </View>
 )
 
-const styles = StyleSheet.create({
-    separator: {
-        flex: 8,
-        flexDirection: "row",
-        borderColor: "#7c7fca",
-        borderWidth: 0.7,
-    },
-})
 
 export default ContentSeparator
